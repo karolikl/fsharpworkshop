ea3f2ea0fd1cb14ad25c6fa1eaf7d2a0b72cedff
on <olavnybo@gmail.com>
2015-04-12 15:43:05 +0200

    adding partial application and composition

---
+++ PresentationCode/FSharpWorkshop/Composition.fs
@@ -0,0 +1,7 @@
+﻿module Composition
+
+let add4 a = a + 4
+let add5 a = a + 5
+let add9 = add4 >> add5
+
+let result = add9 1
---
+++ PresentationCode/FSharpWorkshop/PartialApplication.fs
@@ -0,0 +1,6 @@
+﻿module PartialApplication
+
+let sum a b = a + b
+let result1 = sum 1 2
+let add5 = sum 5
+let result2 = add5 7
